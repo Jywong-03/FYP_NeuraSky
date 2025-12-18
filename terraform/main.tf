af9c1c096eef30b854bb9d6630e51ea3ef5493d0
Jywong-03 <jywong75@hotmail.com>
2025-12-18 17:01:10 +0800

    Update instance type

--- terraform/main.tf
+++ terraform/main.tf
@@ -144,7 +144,7 @@ data "aws_ami" "ubuntu" {
 # The EC2 Instance
 resource "aws_instance" "app_server" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.neurasky_subnet.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
